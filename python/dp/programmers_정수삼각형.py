12668cda32a82fc699e86ab366dc9eaffc2e45f0
Goathoon <95288696+Goathoon@users.noreply.github.com>
2023-01-08 11:57:51 +0900

    Update programmers_정수삼각형.py

--- python/dp/programmers_정수삼각형.py
+++ python/dp/programmers_정수삼각형.py
@@ -15,3 +15,15 @@ def solution(triangle):
         memo.clear()
                 
     return max(triangle[len(triangle)-1])
+
+# copy 없이 이렇게도 가능 (내가 하려고했던것)
+def solution(triangle):
+    for t in range(1, len(triangle)):
+        for i in range(t+1):
+            if i == 0:
+                triangle[t][0] += triangle[t-1][0]
+            elif i == t:
+                triangle[t][-1] += triangle[t-1][-1]
+            else:
+                triangle[t][i] += max(triangle[t-1][i-1], triangle[t-1][i])
+    return max(triangle[-1])
