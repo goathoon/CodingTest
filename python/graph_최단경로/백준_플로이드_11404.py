8940bd301583a18787722bb4f471312f368f066d
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-16 02:02:57 +0900

    플로이드는 "거쳐가는 노드"기준으로 3중반복문이다.

---
+++ python/graph_최단경로/백준_플로이드_11404.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+city = int(input())
+bus = int(input())
+
+city_map = [[987654321]*(city+1) for _ in range(city+1)]
+for _ in range(bus):
+    start,end,cost = map(int,input().split())
+    city_map[start][end] = min(city_map[start][end],cost)
+for i in range(1,city+1):
+    city_map[i][i] = 0
+    
+for via in range(1,city+1):
+    for start in range(1,city+1):
+        for end in range(1,city+1):
+            city_map[start][end] = min(city_map[start][end],city_map[start][via]+city_map[via][end])
+
+for i in range(1,city+1):
+    for j in range(1,city+1):
+        if city_map[i][j] == 987654321:
+            print(0,end=' ')
+        else :
+            print(city_map[i][j],end = ' ')
+        if j == city:
+            print('')
