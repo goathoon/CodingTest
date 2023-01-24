09fd145b13921dc71eb57a1dbd58dc5c63844c8d
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-25 00:57:57 +0900

    백준 (덱) 큐변형(pop,poleft)

---
+++ python/Stack_Queue/백준_덱(큐변형).py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+case = int(input())
+q = deque()
+
+for _ in range(case):
+    command = input().split()
+    if command[0] == "push_back":
+        q.append(command[1])
+    elif command[0] == "push_front":
+        q.appendleft(command[1])
+    elif command[0] == "front":
+        if not q:
+            print("-1")
+        else :
+            print(q[0])
+    elif command[0] == "back":
+        if not q:
+            print("-1")
+        else :
+            print(q[-1])
+    elif command[0] == "size":
+        print(len(q))
+    elif command[0] == "pop_front":
+        if not q :
+            print("-1")
+        else :
+            print(q.popleft())
+    elif command[0] == "pop_back":
+        if not q :
+            print("-1")
+        else :
+            print(q.pop())
+    elif command[0] == "empty":
+        if not q :
+            print("1")
+        else:
+            print("0")
