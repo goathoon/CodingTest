ef71409ce38c06aea1d9784285e8e8978aa2ad03
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-31 22:29:47 +0900

    수정이요..브루트포스

--- python/BruteForce/백준_리모콘_1107.py
+++ python/BruteForce/백준_리모콘_1107.py
@@ -1,8 +1,12 @@
 target = input()
 num = int(input())
 num_list = [i for i in range(10)]
-dif_list = list(map(int,input().split()))
-valid_list = list(set(num_list)-set(dif_list))
+
+if(num!=0):
+    dif_list = list(map(int,input().split()))
+    valid_list = list(set(num_list)-set(dif_list))
+else :
+    valid_list=num_list
 answer = abs(int(target) - 100)
 answer_num = 0
 for i in range(1000001):
