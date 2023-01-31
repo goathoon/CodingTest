28b96cfdc53800e7c4b710b2c938631ad7343c1e
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-31 22:22:23 +0900

    BruteForce는 어려워

---
+++ python/BruteForce/백준_리모콘_1107.py
@@ -0,0 +1,15 @@
+target = input()
+num = int(input())
+num_list = [i for i in range(10)]
+dif_list = list(map(int,input().split()))
+valid_list = list(set(num_list)-set(dif_list))
+answer = abs(int(target) - 100)
+answer_num = 0
+for i in range(1000001):
+    i = str(i)
+    for j in range(len(i)):
+        if int(i[j]) not in valid_list:
+            break
+        elif j == len(i) - 1:
+            answer = min(answer, len(i)+ abs(int(i) - int(target)))
+print(answer)
