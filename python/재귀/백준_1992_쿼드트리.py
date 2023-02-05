b2e2b61aeb58580878df6c79b7efa0fb670baed6
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-02-05 13:27:11 +0900

    수정 ㄱㄱ

---
+++ python/재귀/백준_1992_쿼드트리.py
@@ -0,0 +1,24 @@
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int,input())))
+answer = []
+def quad(row,col,N):
+    if N==1:
+        answer.append(graph[row][col])
+        return
+    num = graph[row][col]
+    for i in range(row,N):
+        for j in range(col,N):
+            if graph[i][j] != num:
+                answer.append('(')
+                quad(row,col,N//2)
+                quad(row,col+N//2,N//2) #for문을 앞에거 계쇽 받음
+                quad(row+N//2,col,N//2)
+                quad(row+N//2,col+N//2,N//2)
+                answer.append(')')
+                return
+    answer.append((graph[row][col]))
+quad(0,0,N)
+for i in range(len(answer)):
+    print(answer[i],end='')
