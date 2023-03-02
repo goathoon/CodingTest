a19d942b851c4fc986fd1efb26749fdbf2694df2
Goathoon <ownest2@gmail.com>
2023-03-03 00:50:14 +0900

    brute force로 재귀의 추가적 풀이

--- python/BruteForce/프로그래머스_마법의엘레베이터.py
+++ python/BruteForce/프로그래머스_마법의엘레베이터.py
@@ -17,3 +17,20 @@ def dfs(count, num, sheet):
     dfs(count+first, num//10, sheet)
     second = 10-first
     dfs(count+second, (num+10)//10, sheet)
+
+# 추가 풀이
+
+
+def solution(storey):
+    answer = 0
+
+    while storey:
+        div = storey % 10
+        if div == 5 and storey//10 % 10 >= 5 or div > 5:
+            storey += 10-div
+            answer += 10-div
+        else:
+            answer += div
+        storey = storey//10
+
+    return answer
