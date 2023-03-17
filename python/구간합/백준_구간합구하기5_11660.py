f38984c4967d26b9347ddfbf41c0468854cd3663
Goathoon <ownest2@gmail.com>
2023-03-17 18:14:59 +0900

    2차원 배열 구간합 문제 재밌었지만..힘들었던 구현이었다.

--- python/구간합/백준_구간합구하기5_11660.py
+++ python/구간합/백준_구간합구하기5_11660.py
@@ -1,24 +1,19 @@
-N, num = map(int, input().split())
-grid = []
-sum_grid = [[0]*N for _ in range(N)]
-for _ in range(N):
-    grid.append(list(map(int, input().split())))
-sum_grid[0][0] = grid[0][0]
+import sys
+input = sys.stdin.readline
 
-for row in range(N):
-    for col in range(N):
-        if row > 0:
-            if col > 0:
-                sum_grid[row][col] = sum_grid[row-1][col] + \
-                    sum_grid[row][col-1] + grid[row][col]
-            else:
-                sum_grid[row][col] = sum_grid[row-1][col] + grid[row][col]
-        else:
-            if col == 0:
-                continue
-            else:
-                sum_grid[row][col] = sum_grid[row][col-1] + grid[row][col]
-for _ in range(num):
+N, M = map(int, input().split())
+grid = [[0] * (N+1)]
+sum_grid = [[0]*(N+1) for _ in range(N+1)]
+for i in range(N):
+    grid.append([0] + list(map(int, input().split())))  # 0으로 추가하기.
+    # 그래야 쉽게 result부분에서 생길 index out of range를 해결할 수 있다. + 계산에 영향을 주지 않기위해
+    # 0으로 초기화
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        sum_grid[i][j] = sum_grid[i-1][j] + \
+            sum_grid[i][j-1] - sum_grid[i-1][j-1] + grid[i][j]
+for i in range(M):
     s_r, s_c, d_r, d_c = map(int, input().split())
-
-print(sum_grid)
+    ans = sum_grid[d_r][d_c] - sum_grid[s_r-1][d_c] - \
+        sum_grid[d_r][s_c-1] + sum_grid[s_r-1][s_c-1]
+    print(ans)
