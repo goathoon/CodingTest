22e6b723ab24686da4edb5b5f1e2919fc69fcfec
Goathoon <ownest2@gmail.com>
2023-03-29 17:29:23 +0900

    수정해줘..

--- python/DFS_BFS/백준_치킨배달_15686.py
+++ python/DFS_BFS/백준_치킨배달_15686.py
@@ -12,10 +12,17 @@ for row in range(1,n+1):
         elif val == 1:
             house_arr.append((row,col))
     grid.append(append_list) 
-ans = 987654321
+ans = 1000000
+
 for tup in combinations(chick_arr,m): #튜플형태이다.
-    
     for house in house_arr:
+        x,y= house
+        cand_ans = 0
         for chick in tup:
-            
+            part_ans=10000
+            dist = abs(x-chick[0])+abs(y-chick[1])
+            part_ans = min(part_ans,dist)
+        cand_ans += part_ans
+    ans = min(ans,cand_ans)
+print(ans)
 
