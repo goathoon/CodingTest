b9c3e6cf82a8b118314dd10f1749750de44ad57f
Goathoon <ownest2@gmail.com>
2023-11-26 17:55:51 +0900

    약수 구현하기

---
+++ java/약수/pg_기사단원의무기.java
@@ -0,0 +1,29 @@
+import java.util.stream.*;
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] numbers = IntStream.rangeClosed(1,number).toArray();
+        for(int n:numbers){
+            int cnt = getCntDivisor(n) ;
+            if (cnt> limit){
+                answer += power;
+            }
+            else{
+                answer += cnt;
+            }
+                
+        }
+        return answer;
+    }
+    public static int getCntDivisor(int n){
+        int cnt = 0;
+        for (int i = 1; i <= (int)Math.sqrt(n); i++){
+            if (n % i == 0) {
+                if (n/i ==i) cnt ++;
+                else cnt += 2;
+            }
+        }
+        return cnt;
+    }
+}
+
