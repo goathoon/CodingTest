ad13f8a16bd8d0193be74ee28dcff1147955080e
Goathoon <ownest2@gmail.com>
2024-01-18 23:09:18 +0900

    슬라이딩 윈도우 & 큐와 해시테이블로 푼 문제

--- java/슬라이딩윈도우/leet_3.java
+++ java/슬라이딩윈도우/leet_3.java
@@ -1,6 +1,5 @@
-package 큐;
+package 슬라이딩윈도우;
 import java.util.*;
-
 /**
  * Given a string s, find the length of the longest
  * substring
@@ -26,29 +25,24 @@ import java.util.*;
  * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class leet_3 {
-
     class Solution {
         public int lengthOfLongestSubstring(String s) {
+            int l = 0, r = 0, len = s.length(), curLen = 0, maxLen = 0;
             Set<Character> set = new HashSet<>();
-            Queue<Character> q = new LinkedList<>();
-
-            char[] arr = s.toCharArray();
-            int answer = 0;
-            for(char c: arr){
-                if(set.contains(c)){
-                    while(q.peek() != c){
-                        char p = q.poll();
-                        set.remove(p);
-                    }
-                    q.poll();
-                    set.remove(c);
+            while(r < len){
+                char c = s.charAt(r);
+                if(!set.contains(c)){
+                    set.add(c);
+                    curLen = r - l + 1;
+                    r++;
+                    maxLen = Math.max(curLen,maxLen);
                 }
-                set.add(c);
-                q.add(c);
-                answer = Math.max(q.size(),answer);
+                else{
+                    set.remove(s.charAt(l));
+                    l++;
+                }
             }
-
-            return answer;
+            return maxLen;
         }
     }
 }
