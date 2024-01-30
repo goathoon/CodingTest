efa06a3e457c768502eaf84736c1966947a5ab32
Goathoon <ownest2@gmail.com>
2024-01-30 16:37:47 +0900

    다익스트라를 활용 역시 create generic array 오류로 약간의 고생을 했다 + Object -> Integer의 형 변환은 필수이다.

---
+++ java/다익스트라/pg_가장먼노드.java
@@ -0,0 +1,55 @@
+package 다익스트라;
+import java.util.*;
+public class pg_가장먼노드 {
+
+    class Solution {
+        static boolean[] visited;
+        public int solution(int n, int[][] edge) {
+            List[] list = new ArrayList[n+1];
+            visited = new boolean[n+1];
+
+            PriorityQueue<Node> pq = new PriorityQueue<>();
+            for(int i = 0; i <= n; i++){
+                list[i] = new ArrayList<Integer>();
+            }
+            for(int[] e: edge){
+                int v1 = e[0];
+                int v2 = e[1];
+                list[v1].add(v2);
+                list[v2].add(v1);
+            }
+            pq.add(new Node(1,0));
+            visited[1] = true;
+            int maxDist = 0;
+            int answer = 0;
+            while(!pq.isEmpty()){
+                Node node = pq.poll();
+                int curV = node.v;
+                int curD = node.dist;
+                if(maxDist < curD){
+                    maxDist = curD;
+                    answer = 0;
+                }
+                answer ++;
+                for(Object nextV: list[curV]){
+                    if(!visited[(int)nextV]){
+                        pq.add(new Node((int)nextV,curD+1));
+                        visited[(int)nextV] = true;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+    class Node implements Comparable<Node>{
+        int v;
+        int dist;
+        Node(int v, int dist){
+            this.v = v;
+            this.dist = dist;
+        }
+        public int compareTo(Node n){
+            return this.dist - n.dist;
+        }
+    }
+}
