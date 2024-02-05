e154f828fc53e1039d64e25eab38761e2d90b923
Goathoon <ownest2@gmail.com>
2024-02-05 21:38:50 +0900

    같은 문제를 Top-Down으로 풀기. 두가지 방법 모두 복습을 잘할 것

--- java/DP/leet_1043.java
+++ java/DP/leet_1043.java
@@ -21,8 +21,10 @@ package DP;
  * Input: arr = [1], k = 1
  * Output: 1
  */
+import java.util.*;
 public class leet_1043 {
-    class Solution {
+    //Bottom-up
+    class Solution1 {
         // O(n*k)
         // i번째 index부터 i+k 까지 돌면서 최대값 찾고, 해당 subarray 길이만큼 최대값을 곱한후, i-1번째 dp 에 최대값 추가시키면 된다.
         public int maxSumAfterPartitioning(int[] arr, int k) {
@@ -37,6 +39,30 @@ public class leet_1043 {
                 }
             }
             return dp[arr.length-1];
+        }
+    }
+    //Top-down
+    class Solution2 {
+        static Map<Integer,Integer> map;
+        public int maxSumAfterPartitioning(int[] arr, int k) {
+            map = new HashMap<>();
+            return dfs(0,arr,k);
+        }
+        public int dfs(int i, int[] arr, int k){
+            if(i == arr.length){
+                return 0;
+            }
+            if(map.containsKey(i)){
+                return map.get(i);
+            }
+            int curMax = 0;
+            int curVal = 0;
+            for(int j = i; j < Math.min(i+k,arr.length); j++){
+                curMax = Math.max(curMax,arr[j]);
+                curVal = Math.max(curVal,(j-i+1) * curMax + dfs(j+1,arr,k));
+            }
+            map.put(i,curVal);
+            return curVal;
         }
     }
 }
