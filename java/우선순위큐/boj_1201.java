ac3bb8cc8f856f21719514ebe6bdfa83dac57b19
Goathoon <ownest2@gmail.com>
2024-02-06 18:29:16 +0900

    그리디하게 하려고 하지만 시간복잡도를 위하여 어떤 전략을 취해야할까? <우선순위 큐>

---
+++ java/우선순위큐/boj_1201.java
@@ -0,0 +1,56 @@
+package 우선순위큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_1201 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Jewel> list = new ArrayList<>();
+        int [] bags = new int[K];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int M = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            list.add(new Jewel(M, V));
+        }
+        for(int i = 0; i < K; i++){
+            int C = Integer.parseInt(br.readLine());
+            bags[i] = C;
+        }
+        Arrays.sort(bags);
+        Collections.sort(list);
+        long answer = 0;
+        int jewelIdx = 0;
+        PriorityQueue<Jewel> pq = new PriorityQueue<>((o1,o2) ->(o2.v - o1.v));
+
+        for(int i = 0; i < K; i++){
+            while(jewelIdx < N && list.get(jewelIdx).m <= bags[i]){
+                pq.add(list.get(jewelIdx++));
+            }
+            if(!pq.isEmpty())  {
+                Jewel jewel = pq.poll();
+                answer += jewel.v;
+            }
+        }
+        System.out.println(answer);
+    }
+    static class Jewel implements Comparable<Jewel>{
+        int m;
+        int v;
+        public Jewel(int m, int v){
+            this.m = m;
+            this.v = v;
+        }
+        public int compareTo(Jewel jewel){
+            return this.m - jewel.m;
+        }
+    }
+}
