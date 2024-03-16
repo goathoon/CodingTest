19cb413d14d25b381684f89bdf1f11486afaa414
Goathoon <ownest2@gmail.com>
2024-03-17 01:01:38 +0900

    위상정렬에 PQ대신 DP도 도입하여 푼 문제

--- java/위상정렬/boj_1005.java
+++ java/위상정렬/boj_1005.java
@@ -61,4 +61,64 @@ public class boj_1005 {
         }
 
     }
+    class Solution2{
+        public static void main(String[] args) throws IOException {
+
+
+            class Building {
+                int num;
+                int cost;
+
+                Building(int num, int cost) {
+                    this.num = num;
+                    this.cost = cost;
+                }
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int TC = Integer.parseInt(br.readLine());
+            for (int t = 0; t < TC; t++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                int N = Integer.parseInt(st.nextToken());
+                int K = Integer.parseInt(st.nextToken());
+                int[] build = new int[N + 1];
+                int[] answers = new int[N + 1];
+                int[] indegree = new int[N + 1];
+                st = new StringTokenizer(br.readLine(), " ");
+                List<List<Integer>> graph = new ArrayList<>();
+                graph.add(new ArrayList<>());
+                for (int i = 1; i <= N; i++) {
+                    graph.add(new ArrayList<>());
+                    build[i] = Integer.parseInt(st.nextToken());
+                    answers[i] = build[i];
+                }
+                for (int i = 0; i < K; i++) {
+                    st = new StringTokenizer(br.readLine(), " ");
+                    int start = Integer.parseInt(st.nextToken());
+                    int end = Integer.parseInt(st.nextToken());
+                    graph.get(start).add(end);
+                    indegree[end]++;
+                }
+                int target = Integer.parseInt(br.readLine());
+                Queue<Building> q = new LinkedList<>();
+                for (int i = 1; i <= N; i++) {
+                    if (indegree[i] == 0) {
+                        q.add(new Building(i, build[i]));
+                    }
+                }
+                while (!q.isEmpty()) {
+                    Building curBuilding = q.poll();
+                    int curNum = curBuilding.num;
+                    int curCost = curBuilding.cost;
+                    for (int next : graph.get(curNum)) {
+                        indegree[next]--;
+                        answers[next] = Math.max(answers[next], curCost + build[next]);
+                        if (indegree[next] == 0) {
+                            q.add(new Building(next, answers[next]));
+                        }
+                    }
+                }
+                System.out.println(answers[target]);
+            }
+        }
+    }
 }
