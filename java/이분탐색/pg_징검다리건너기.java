e1e7d49840169143708d26beade0360b2bafcf9e
Goathoon <ownest2@gmail.com>
2024-03-22 03:21:20 +0900

    이분탐색을 등호있는버전과 없는버전으로 나누어 풀었다

--- java/이분탐색/pg_징검다리건너기.java
+++ java/이분탐색/pg_징검다리건너기.java
@@ -32,4 +32,37 @@ public class pg_징검다리건너기 {
         }
 
     }
+    class Solution2 {
+        public int solution(int[] stones, int k) {
+            long l = 0;
+            long r = Integer.MAX_VALUE;
+            int ans = 0;
+            while(l <= r){
+                long m = (l+r)/2;
+                int cnt = 0;
+                int maxCnt = 0;
+                for(int s : stones){
+                    if (s-m <= 0){
+                        cnt++;
+                    }
+                    else {
+                        maxCnt= Math.max(maxCnt,cnt);
+                        cnt = 0;
+                    }
+                }
+
+                maxCnt= Math.max(maxCnt,cnt);
+
+                if(maxCnt < k){
+                    l = m+1;
+                    ans = Math.max((int)l,ans);
+                }
+                else{
+                    r = m-1;
+                }
+            }
+            return ans;
+        }
+
+    }
 }
