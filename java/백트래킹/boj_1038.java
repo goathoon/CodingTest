ef0501a28aaddd6c19f08a0916b8958699b1b893
Goathoon <ownest2@gmail.com>
2024-04-30 19:07:20 +0900

    [추천] 백트래킹의 조건을 이해해야만 풀 수 있는 문제다. 추가적으로, N의 개수가 1,000,000이므로, 전체 value를 다 추가하고, sort해도 상관없다. 해당 풀이를 추가함

--- java/백트래킹/boj_1038.java
+++ java/백트래킹/boj_1038.java
@@ -47,4 +47,45 @@ public class boj_1038 {
             return -1;
         }
     }
+
+    /**
+     *  https://g-db.tistory.com/entry/JAVA-%EB%B0%B1%EC%A4%80-1038 출처
+     */
+    class Solution2 {
+        static int N;
+        static List<Long> A = new ArrayList<>();
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+
+            if(N <= 10){
+                System.out.println(N);
+                return ;
+            }
+
+            for(int i=1; i<10; i++){
+                search(i, 1);
+            }
+            if(A.size() < N){
+                System.out.println("-1");
+                return;
+            }
+
+            Collections.sort(A);
+            System.out.println(A.get(N-1));
+
+        }
+
+        public static void search(long value, int th){
+            if(th > 10) return ;
+
+            A.add(value);
+            for(int i=0; i<value%10; i++){
+                search(value*10 + i, th+1);
+            }
+
+        }
+
+    }
 }
