9019b98bd70563100fa33654d34533539935c6a9
Goathoon <ownest2@gmail.com>
2024-05-02 01:29:39 +0900

    [추천] 이분탐색의 아이디어. 이분탐색의 구현. 항상 생각할게 많다

---
+++ java/이분탐색/boj_6209.java
@@ -0,0 +1,46 @@
+package 이분탐색;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_6209 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int end = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+
+        // 전체 징검다리 위치별로 정렬
+        int[] stones = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            stones[i] = x;
+        }
+        stones[n] = end;
+        Arrays.sort(stones);
+
+        int left = 0, right = end;
+        while(left <= right){
+            int mid = (left+right)/2;
+            int steps = countNumbers(mid,stones);
+            if(steps < n-m+1){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        System.out.println(left-1);
+    }
+    public static int countNumbers(int mid, int[] stones){
+        int curPos = 0;
+        int nextIdx = 0;
+        int res = 0;
+        while(nextIdx < stones.length){
+            if(stones[nextIdx] - curPos >= mid){
+                res++;
+                curPos = stones[nextIdx];
+            }
+            nextIdx++;
+        }
+        return res;
+    }
+}
