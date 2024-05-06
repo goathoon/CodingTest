ac3597dedb7528c567adfb27799222f060823c9e
Goathoon <ownest2@gmail.com>
2024-05-06 19:54:01 +0900

    [복습 필] 배낭문제 축약

--- java/DP/boj_12865.java
+++ java/DP/boj_12865.java
@@ -8,19 +8,13 @@ public class boj_12865 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
-        int[][] things = new int[N+1][2];
-        for(int n = 1; n <= N; n++){
-            st = new StringTokenizer(br.readLine(), " ");
-            things[n][0] = Integer.parseInt(st.nextToken());
-            things[n][1] = Integer.parseInt(st.nextToken());
-        }
         // K는 무게, N은 물건 개수
         int[][] dp = new int[K+1][N+1];
 
-        // i 번째 물건까지 선택을 완료하였을 때, j 무게 까지 담을 수 있는 최대 가치합
         for(int i = 1; i <= N; i++){
-            int weight = things[i][0];
-            int value = things[i][1];
+            st = new StringTokenizer(br.readLine(), " ");
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
             for(int j = 1; j <= K; j++){
                 // 현재 무게가 i번째 물건으로 채울 수 있는 상황일 때
                 if (j >= weight){
