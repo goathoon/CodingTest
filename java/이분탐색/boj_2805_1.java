f2815f3087451edd793c1a971bdb30aad73241ee
Goathoon <ownest2@gmail.com>
2024-05-28 17:49:46 +0900

    * 이분탐색에서 꼭 답을 도출할 필요는 없다. ans만 탐색마다 저장하는 방식이 더 쉽다

--- java/이분탐색/boj_2805_1.java
+++ java/이분탐색/boj_2805_1.java
@@ -4,30 +4,62 @@ import java.io.*;
 import java.util.*;
 
 public class boj_2805_1 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+    class 답을탐색마다갱신 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            long ans = 0;
+            long start = 0;
+            long end = 1_000_000_000;
+            while (start <= end) {
+                long mid = (start + end) / 2;
+                long sum = 0;
+                for (int x : arr) {
+                    if (x - mid > 0) sum += x - mid;
+                }
+                if (sum >= M) {
+                    start = mid + 1;
+                    ans = mid;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            System.out.println(ans);
         }
-        Arrays.sort(arr);
-        long start = 0; long end = 1_000_000_000;
-        while(start <= end){
-            long mid = (start+end) / 2;
-            long sum = 0;
-            for(int x : arr){
-                if(x - mid  > 0) sum += x - mid;
+    }
+    class MySol {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            if(sum >= M){
-                start = mid + 1;
+            Arrays.sort(arr);
+            long start = 0;
+            long end = 1_000_000_000;
+            while (start <= end) {
+                long mid = (start + end) / 2;
+                long sum = 0;
+                for (int x : arr) {
+                    if (x - mid > 0) sum += x - mid;
+                }
+                if (sum >= M) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
-            else{
-                end = mid - 1;
-            }
+            System.out.println(end);
         }
-        System.out.println(end);
     }
 }
