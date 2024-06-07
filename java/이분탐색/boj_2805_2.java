d9e660855ec671c08b07c128a4b592b1e8602686
Goathoon <ownest2@gmail.com>
2024-06-08 01:17:15 +0900

    5/28자 복습

--- java/이분탐색/boj_2805_2.java
+++ java/이분탐색/boj_2805_2.java
@@ -8,30 +8,61 @@ public class boj_2805_2 {
         int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        int ans = 0;
+
         int l = 0, r = 2_000_000_001;
-        while(l <= r){
-            int mid = (l+r)/2;
+        while (l < r) {
+            int mid = (l + r) / 2;
             long sum = 0;
-            for(int tree: arr){
-                if(tree - mid > 0){
+            for (int tree : arr) {
+                if (tree - mid > 0) {
                     sum += tree - mid;
                 }
             }
-            if(sum >= M){
-                ans = Math.max(ans,mid);
-                l = mid+1;
-            }
-            else{
-                r = mid-1;
+            if (sum >= M) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
-        System.out.println(ans);
+        System.out.println(l-1);
 
     }
+    class SimpleSol {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            int ans = 0;
+            int l = 0, r = 2_000_000_001;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                long sum = 0;
+                for (int tree : arr) {
+                    if (tree - mid > 0) {
+                        sum += tree - mid;
+                    }
+                }
+                if (sum >= M) {
+                    ans = Math.max(ans, mid);
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
 }
